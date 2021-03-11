893df3eeeaaa2988cb8426a23d2580d88358a049
Leonardo Marinho <leommjr09@gmail.com>
2021-03-11 02:04:50 -0300

    UI simples para a MainActivity criada/ event handler de teste adicionado

--- app/src/main/java/com/vulnerable/fakecall/MainActivity.java
+++ app/src/main/java/com/vulnerable/fakecall/MainActivity.java
@@ -3,6 +3,10 @@ package com.vulnerable.fakecall;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public String NUMERO_PADRAO = "(34)984442025";
+
+    public void ligar(View view) {
+        EditText numero = (EditText) findViewById(R.id.inputNumber);
+        Log.e("aaaaaaaaa", String.valueOf(numero));
     }
 }
